14e87e23d06a324670fa5d1db654aad3dd103583
SergeyYakimov <teriertis@yandex.ru>
2019-11-07 00:53:50 +0800

    Made templates of pages

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,52 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty-layout' },
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Categories.vue'),
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Detail.vue'),
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/History.vue'),
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Planning.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Record.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty-layout' },
+    component: () => import('../views/Register.vue'),
   },
 ];
 
