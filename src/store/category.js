5920835fc12267ec6dcc855b4c95cda0c0a0affb
SergeyYakimov <teriertis@yandex.ru>
2019-11-11 17:01:58 +0800

    Made component for editing of categories

--- src/store/category.js
+++ src/store/category.js
@@ -2,11 +2,39 @@ import firebase from 'firebase/app'
 
 export default {
   actions: {
+    async fetchCategories({commit, dispatch}) {
+      try {
+        const uid = await dispatch('getUId');
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {};
+
+        /*const catgs = [];
+        Object.keys(categories).forEach(key => {
+          catgs.push({title:categories[key].title,
+            limit: categories[key].limit, id: key})
+        });
+        return catgs;*/
+        return Object.keys(categories).map(key => ({
+          ...categories[key], id: key
+        }));
+      } catch(e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
     async createCategory({commit, dispatch}, {title, limit}) {
       try {
         const uid = await dispatch('getUId');
         const category = await firebase.database().ref(`/users/${uid}/categories`).push({title,limit});
         return {title,limit, id: category.key};
+      } catch(e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
+    async updateCategory({commit, dispatch}, {title, limit, id}) {
+      try {
+        const uid = await dispatch('getUId');
+        const category = await firebase.database().ref(`/users/${uid}/categories`).child(id).update({title, limit});
       } catch(e) {
         commit('setError', e);
         throw e;
